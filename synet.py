95fd10814ee08a487eadb3ec89720b47a7680998
mjoh223 <mjoh223@lsu.edu>
2019-02-01 11:35:46 -0800

    added xml parser

--- synet.py
+++ synet.py
@@ -1,7 +1,25 @@
-from Bio.Blast.Applications import NcbiblastpCommandline
-target = "/netapp/home/mjohnson/synet/targets/mouse"
-query = "/netapp/home/mjohnson/synet/queries/4TNV.fas"
+from Bio.Blast.Applications import NcbiblastnCommandline
+from Bio.Blast import NCBIXML
 
-blastp_cline = NcbiblastpCommandline(query=query, db=target, evalue=0.001,outfmt=5, out="out.xml")
+target = "/netapp/home/mjohnson/targets/mouse"
+query = "/netapp/home/mjohnson/queries/sample.fas"
 
-blastp_cline()
+blastn_cline = NcbiblastnCommandline(query=query, db=target, evalue=0.01,outfmt=5, out="out.xml")
+
+blastn_cline()
+
+result_handle = open("out.xml")
+blast_record = NCBIXML.read(result_handle)
+
+E_VALUE_THRESH = 0.01
+
+for alignment in blast_record.alignments:
+	for hsp in alignment.hsps:
+		if hsp.expect < E_VALUE_THRESH:
+			print("****Alignment****")
+			print("sequence:", alignment.title)
+			print("length:", alignment.length)
+			print("e value:", hsp.expect)
+			print(hsp.query[0:75] + "...")
+			print(hsp.match[0:75] + "...")
+			print(hsp.sbjct[0:75] + "...")
